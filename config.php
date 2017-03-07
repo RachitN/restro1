28dc91e9ef915166657a60506f88470191430526
Rachit Nagwani <rachitnagwani13@gmail.com>
2017-03-07 23:07:01 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
 $u=$_POST['userid'];
 $p=$_POST['pass'];
-$user_name="root";
-$password="bhanbhari";
-$db="rachit_database";
-$c=mysqli_connect('localhost',$user_name,$password,$db);
+$user_name="bc735e9a1d9622";
+$password="47acc5cd";
+$db="rachitdb";
+$c=mysqli_connect('us-cdbr-azure-southcentral-f.cloudapp.net',$user_name,$password,$db);
 ?>
